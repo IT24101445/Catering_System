28cf2c9a05d191bdd5717b9ba63cf798c5beebb8
Chandupa <it24101444@my.sliit.lk>
2025-09-24 16:15:01 +0530

    Initial commit of Spring Boot project

---
+++ catering-system/src/test/java/com/example/catering_system/CateringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catering_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CateringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
